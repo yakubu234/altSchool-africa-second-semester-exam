e9261dae7dd43ee79cc56629512d608b140601e3
yakubu234 <yakubuabiola2003@gmail.com>
2022-11-09 10:24:58 +0100

    removing the comment at blogController

--- app/controllers/BlogController.js
+++ app/controllers/BlogController.js
@@ -93,52 +93,6 @@ async function fetchSingleBlog(req, res, next) {
 
 }
 
-async function updateBlog(req, res, next) {
-
-}
-
-// async function listBlog(req, res, next) {
-//     return res.json(req.body.auth.user._id)
-
-//     limit = 2
-//     const requiredData = matchedData(req, { includeOptionals: false }); // get the validated data from request
-//     page = (requiredData.page) ? requiredData.page : 1; //check if pagenumber is passed
-
-//     requiredData.state = (requiredData.state) ? requiredData.state : BlogState; // check if state is passed else asign value to it
-
-//     delete requiredData.page // delete the page key from the request
-//     const skip = (page - 1) * limit;
-
-//     try {
-//         const blogs = await blogModel.find(requiredData).populate({ path: 'authorDetails' }) // execute query with page and limit values
-//             .limit(limit * 1)
-//             .skip(skip)
-//             .exec();
-
-//         const count = await blogModel.countDocuments(); // get total documents in the blogs collection 
-//         if (skip > count || !blogs) {
-//             err.type = 'Invallid Page';
-//             return next(err)
-//         }
-
-//         // return response with blogs, total pages, and current page
-//         res.status(200).json({
-//             status: "success",
-//             message: "Blog list found!!!",
-//             data: {
-//                 blogs,
-//                 totalPages: Math.ceil(count / limit),
-//                 currentPage: page
-//             }
-//         });
-//     } catch (err) {
-//         err.type = 'No Data';
-//         next(err)
-//     }
-// }
-
-
-
 function authenticate(req, res, next) {
     console.log('i am here')
     return
